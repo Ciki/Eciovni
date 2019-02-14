42df6a43b6812415cc4f2605cdcf59fc62e4b01b
Ciki <matula.m@gmail.com>
2019-02-14 14:30:43 +0100

    round latte filter accepts both Money and numeric value

--- OndrejBrejla/Eciovni/Eciovni.php
+++ OndrejBrejla/Eciovni/Eciovni.php
@@ -18,24 +18,24 @@ use Nette\Bridges\ApplicationLatte\Template;
  */
 class Eciovni
 {
-    /** @var Data|null */
-    private $data;
+	/** @var Data|null */
+	private $data;
 
-    /** @var Template */
-    private $template;
+	/** @var Template */
+	private $template;
 
-    /** @var string */
-    private $templatePath;
+	/** @var string */
+	private $templatePath;
 
 
 	public function __construct(?Data $data, ITemplateFactory $templateFactory,
 		LinkGenerator $linkGenerator)
 	{
 		if ($data !== null) {
-            $this->setData($data);
-        }
+			$this->setData($data);
+		}
 
-        $this->templatePath = __DIR__ . '/Eciovni.latte';
+		$this->templatePath = __DIR__ . '/Eciovni.latte';
 
 		/** @var Template $template */
 		$template = $templateFactory->createTemplate();
@@ -45,227 +45,231 @@ class Eciovni
 	}
 
 
-    public function setTemplatePath(string $templatePath): void
-    {
-        $this->templatePath = $templatePath;
-    }
+	public function setTemplatePath(string $templatePath): void
+	{
+		$this->templatePath = $templatePath;
+	}
 
 
-    /**
-     * Exports Invoice template via passed mPDF.
-     */
+	/**
+	 * Exports Invoice template via passed mPDF.
+	 */
 	public function exportToPdf(Mpdf $mpdf, ?string $name = null,
 		?string $dest = null): ?string
-    {
-        $this->generate($this->template);
-        $mpdf->WriteHTML((string) $this->template);
+	{
+		$this->generate($this->template);
+		$mpdf->WriteHTML((string) $this->template);
 
-        $result = null;
-        if (($name !== '') && ($dest !== null)) {
-            $result = $mpdf->Output($name, $dest);
-        } elseif ($dest !== null) {
-            $result = $mpdf->Output('', $dest);
-        } else {
-            $result = $mpdf->Output($name, $dest);
-        }
-        return $result;
-    }
-
-
-    public function render(): void
-    {
-        $this->processRender();
-    }
+		$result = null;
+		if (($name !== '') && ($dest !== null)) {
+			$result = $mpdf->Output($name, $dest);
+		} elseif ($dest !== null) {
+			$result = $mpdf->Output('', $dest);
+		} else {
+			$result = $mpdf->Output($name, $dest);
+		}
+		return $result;
+	}
 
 
-    /**
-     * Renderers the invoice to the defined template.
-     */
-    public function renderData(Data $data): void
-    {
-        $this->setData($data);
-        $this->processRender();
-    }
+	public function render(): void
+	{
+		$this->processRender();
+	}
 
 
-    /**
-     * Renderers the invoice to the defined template.
-     */
-    private function processRender(): void
-    {
-        $this->generate($this->template);
-        $this->template->render();
-    }
+	/**
+	 * Renderers the invoice to the defined template.
+	 */
+	public function renderData(Data $data): void
+	{
+		$this->setData($data);
+		$this->processRender();
+	}
 
 
-    /**
-     * Sets the data, but only if it hasn't been set already.
-     */
+	/**
+	 * Renderers the invoice to the defined template.
+	 */
+	private function processRender(): void
+	{
+		$this->generate($this->template);
+		$this->template->render();
+	}
+
+
+	/**
+	 * Sets the data, but only if it hasn't been set already.
+	 */
 	public function setData(Data $data): void
-    {
-        if ($this->data !== null) {
-            throw new IllegalStateException('Data have already been set!');
-        }
+	{
+		if ($this->data !== null) {
+			throw new IllegalStateException('Data have already been set!');
+		}
 
-	    $this->data = $data;
-    }
+		$this->data = $data;
+	}
 
 
-    /**
-     * Generates the invoice to the defined template.
-     */
-    private function generate(Template $template): void
-    {
-        $template->setFile($this->templatePath);
-        $template->getLatte()->addFilter('round', function(Money $value, $precision = 2) {
-            return number_format(round($value->getAmount() / 100, $precision), $precision, ',', '');
-        });
+	/**
+	 * Generates the invoice to the defined template.
+	 */
+	private function generate(Template $template): void
+	{
+		$template->setFile($this->templatePath);
+		$template->getLatte()->addFilter('round',
+			function($value, $precision = 2) {
+			if ($value instanceof Money) {
+				$value = $value->getAmount() / 100;
+			}
+			return number_format(round($value, $precision), $precision, ',', '');
+		});
 
-        $template->title = $this->data->getTitle();
-        $template->caption = (string) $this->data->getCaption();
-        $template->id = $this->data->getId();
-        $template->paymentType = $this->data->getPaymentType();
-        $template->items = $this->data->getItems();
+		$template->title = $this->data->getTitle();
+		$template->caption = (string) $this->data->getCaption();
+		$template->id = $this->data->getId();
+		$template->paymentType = $this->data->getPaymentType();
+		$template->items = $this->data->getItems();
 		$template->currency = $template->items[0]->getUnitValue()->getCurrency();
-        $this->generateSupplier($template);
-        $this->generateCustomer($template);
-        $this->generateDates($template);
-        $this->generateSymbols($template);
-        $this->generateFinalValues($template);
-    }
+		$this->generateSupplier($template);
+		$this->generateCustomer($template);
+		$this->generateDates($template);
+		$this->generateSymbols($template);
+		$this->generateFinalValues($template);
+	}
 
 
-    /**
-     * Generates supplier data into template.
-     */
-    private function generateSupplier(Template $template): void
-    {
-        $supplier = $this->data->getSupplier();
-        $template->supplierName = $supplier->getName();
-        $template->supplierStreet = $supplier->getStreet();
-        $template->supplierHouseNumber = $supplier->getHouseNumber();
-        $template->supplierCity = $supplier->getCity();
-        $template->supplierZip = $supplier->getZip();
+	/**
+	 * Generates supplier data into template.
+	 */
+	private function generateSupplier(Template $template): void
+	{
+		$supplier = $this->data->getSupplier();
+		$template->supplierName = $supplier->getName();
+		$template->supplierStreet = $supplier->getStreet();
+		$template->supplierHouseNumber = $supplier->getHouseNumber();
+		$template->supplierCity = $supplier->getCity();
+		$template->supplierZip = $supplier->getZip();
 		$template->supplierCountry = $supplier->getCountry();
-        $template->supplierIn = $supplier->getIn();
-        $template->supplierTin = $supplier->getTin();
+		$template->supplierIn = $supplier->getIn();
+		$template->supplierTin = $supplier->getTin();
 		$template->supplierVatId = $supplier->getVatId();
-	    $template->supplierVatPayer = $supplier->isVatPayer();
+		$template->supplierVatPayer = $supplier->isVatPayer();
 		$template->supplierRegistryInfo = $supplier->getRegistryInfo();
-        $template->supplierAccountNumber = $supplier->getAccountNumber();
-    }
+		$template->supplierAccountNumber = $supplier->getAccountNumber();
+	}
 
 
-    /**
-     * Generates customer data into template.
-     */
-    private function generateCustomer(Template $template): void
-    {
-        $customer = $this->data->getCustomer();
-        $template->customerName = $customer->getName();
-        $template->customerStreet = $customer->getStreet();
-        $template->customerHouseNumber = $customer->getHouseNumber();
-        $template->customerCity = $customer->getCity();
-        $template->customerZip = $customer->getZip();
+	/**
+	 * Generates customer data into template.
+	 */
+	private function generateCustomer(Template $template): void
+	{
+		$customer = $this->data->getCustomer();
+		$template->customerName = $customer->getName();
+		$template->customerStreet = $customer->getStreet();
+		$template->customerHouseNumber = $customer->getHouseNumber();
+		$template->customerCity = $customer->getCity();
+		$template->customerZip = $customer->getZip();
 		$template->customerCountry = $customer->getCountry();
-        $template->customerIn = $customer->getIn();
-        $template->customerTin = $customer->getTin();
+		$template->customerIn = $customer->getIn();
+		$template->customerTin = $customer->getTin();
 		$template->customerVatId = $customer->getVatId();
-        $template->customerVatPayer = $customer->isVatPayer();
+		$template->customerVatPayer = $customer->isVatPayer();
 		$template->customerRegistryInfo = $customer->getRegistryInfo();
-        $template->customerAccountNumber = $customer->getAccountNumber();
-    }
+		$template->customerAccountNumber = $customer->getAccountNumber();
+	}
 
 
-    /**
-     * Generates dates into template.
-     */
-    private function generateDates(Template $template): void
-    {
-        $template->dateOfIssuance = $this->data->getDateOfIssuance();
-        $template->dateOfDelivery = $this->data->getDateOfDelivery();
-        $template->expirationDate = $this->data->getExpirationDate();
-        $template->dateOfVatRevenueRecognition = $this->data->getDateOfVatRevenueRecognition();
-    }
+	/**
+	 * Generates dates into template.
+	 */
+	private function generateDates(Template $template): void
+	{
+		$template->dateOfIssuance = $this->data->getDateOfIssuance();
+		$template->dateOfDelivery = $this->data->getDateOfDelivery();
+		$template->expirationDate = $this->data->getExpirationDate();
+		$template->dateOfVatRevenueRecognition = $this->data->getDateOfVatRevenueRecognition();
+	}
 
 
-    /**
-     * Generates symbols into template.
-     */
-    private function generateSymbols(Template $template): void
-    {
-        $template->variableSymbol = $this->data->getVariableSymbol();
-        $template->specificSymbol = $this->data->getSpecificSymbol();
-        $template->constantSymbol = $this->data->getConstantSymbol();
-    }
+	/**
+	 * Generates symbols into template.
+	 */
+	private function generateSymbols(Template $template): void
+	{
+		$template->variableSymbol = $this->data->getVariableSymbol();
+		$template->specificSymbol = $this->data->getSpecificSymbol();
+		$template->constantSymbol = $this->data->getConstantSymbol();
+	}
 
 
-    /**
-     * Generates final values into template.
-     */
-    private function generateFinalValues(Template $template): void
-    {
-        $template->finalUntaxedValue = $this->countFinalUntaxedValue();
-        $template->finalTaxValue = $this->countFinalTaxValue();
-        $template->finalValue = $this->countFinalValues();
-    }
+	/**
+	 * Generates final values into template.
+	 */
+	private function generateFinalValues(Template $template): void
+	{
+		$template->finalUntaxedValue = $this->countFinalUntaxedValue();
+		$template->finalTaxValue = $this->countFinalTaxValue();
+		$template->finalValue = $this->countFinalValues();
+	}
 
 
-    /**
-     * Counts final untaxed value of all items.
-     */
-    private function countFinalUntaxedValue(): Money
-    {
-        $sum = null;
+	/**
+	 * Counts final untaxed value of all items.
+	 */
+	private function countFinalUntaxedValue(): Money
+	{
+		$sum = null;
 
-        foreach ($this->data->getItems() as $item) {
-        	if ($sum === null) {
-        		$sum = $item->countUntaxedUnitValue()->multiply($item->getUnits());
-	        } else {
-        		$sum = $sum->add($item->countUntaxedUnitValue()->multiply($item->getUnits()));
-	        }
-        }
+		foreach ($this->data->getItems() as $item) {
+			if ($sum === null) {
+				$sum = $item->countUntaxedUnitValue()->multiply($item->getUnits());
+			} else {
+				$sum = $sum->add($item->countUntaxedUnitValue()->multiply($item->getUnits()));
+			}
+		}
 
-        return $sum;
-    }
-
-
-    /**
-     * Counts final tax value of all items.
-     */
-    private function countFinalTaxValue(): Money
-    {
-        $sum = null;
-
-        foreach ($this->data->getItems() as $item) {
-	        if ($sum === null) {
-		        $sum = $item->countTaxValue();
-	        } else {
-		        $sum = $sum->add($item->countTaxValue());
-	        }
-        }
-
-        return $sum;
-    }
+		return $sum;
+	}
 
 
-    /**
-     * Counts final value of all items.
-     */
-    private function countFinalValues(): Money
-    {
-        $sum = null;
+	/**
+	 * Counts final tax value of all items.
+	 */
+	private function countFinalTaxValue(): Money
+	{
+		$sum = null;
 
-        foreach ($this->data->getItems() as $item) {
-	        if ($sum === null) {
-		        $sum = $item->countFinalValue();
-	        } else {
-		        $sum = $sum->add($item->countFinalValue());
-	        }
-        }
+		foreach ($this->data->getItems() as $item) {
+			if ($sum === null) {
+				$sum = $item->countTaxValue();
+			} else {
+				$sum = $sum->add($item->countTaxValue());
+			}
+		}
 
-        return $sum;
-    }
+		return $sum;
+	}
+
+
+	/**
+	 * Counts final value of all items.
+	 */
+	private function countFinalValues(): Money
+	{
+		$sum = null;
+
+		foreach ($this->data->getItems() as $item) {
+			if ($sum === null) {
+				$sum = $item->countFinalValue();
+			} else {
+				$sum = $sum->add($item->countFinalValue());
+			}
+		}
+
+		return $sum;
+	}
 
 
 }
